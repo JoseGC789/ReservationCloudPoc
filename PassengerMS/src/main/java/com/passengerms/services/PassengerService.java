8af3958527babd476ea126f76863bba47fbdb3b0
JoseGC789 <jose.carranza@globant.com>
2019-08-29 21:59:59 -0300

    removed cache logic from this service

--- PassengerMS/src/main/java/com/passengerms/services/PassengerService.java
+++ PassengerMS/src/main/java/com/passengerms/services/PassengerService.java
@@ -2,11 +2,8 @@ package com.passengerms.services;
 
 import com.passengerms.domain.entities.Passenger;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
 
 public interface PassengerService{
-    CompletableFuture<List<Passenger>> retrieve(Long id);
-    default CompletableFuture<List<Passenger>> consume(Long id){
-        throw new UnsupportedOperationException();
-    }
+    List<Passenger> read(Long id);
+    List<Passenger> create(List<Passenger> passengers);
 }
