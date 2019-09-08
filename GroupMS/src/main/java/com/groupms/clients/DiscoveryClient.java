d6b9418c84a4f00ad4309d1cda37e1143bcf7320
JoseGC789 <jose.carranza@globant.com>
2019-09-08 12:54:44 -0300

    integrated with service registry

---
+++ GroupMS/src/main/java/com/groupms/clients/DiscoveryClient.java
@@ -0,0 +1,28 @@
+package com.groupms.clients;
+
+import com.groupms.configs.RestConfig;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestOperations;
+import java.util.Map;
+
+@Service
+public class DiscoveryClient{
+
+    private final DiscoveryProperties properties;
+    private final RestOperations template;
+
+    public DiscoveryClient(DiscoveryProperties properties, RestOperations template){
+        this.properties = properties;
+        this.template = template;
+    }
+
+    public HttpStatus subscribe(String serviceHostName){
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(properties.buildRegistration(serviceHostName).getPayload(), RestConfig.getAcceptHeaders());
+        ResponseEntity response = template.exchange(properties.buildUri(), HttpMethod.POST, entity, String.class);
+        return response.getStatusCode();
+    }
+}
